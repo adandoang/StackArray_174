06da3c7c1b35d741fb7dc283b47a0145caa6b3e3
adandoang <akhdanjzif@gmail.com>
2023-06-16 11:26:15 +0700

    void pop

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -27,5 +27,13 @@ public:
 		return element;
 	}
 
-	void
-};
+	void pop() {
+		if (empty()) {//step 1
+			cout << "stack is empty. cannot pop. " << endl;//1.a
+			return;//1.b
+
+		}
+		cout << "\nthe popped element is:" << stack_array[top] << endl; //step 2
+		top--; //step 3 decrement
+	}
+	
